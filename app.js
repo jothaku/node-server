ab252e516439236d3e8672cb98a25d273eaab4f6
Jothaku <jothagf@gmail.com>
2023-06-14 12:57:51 -0500

    Agregando mas funcionalidades

--- app.js
+++ app.js
@@ -29,40 +29,53 @@ function toggleTask(index) {
   }
 }
 
-console.log("Bienvenido a la gestión de tareas.");
-console.log("Opciones disponibles:");
-console.log("1. Añadir tarea");
-console.log("2. Eliminar tarea");
-console.log("3. Marcar tarea como completada");
+function showOptions() {
+  console.log("Opciones disponibles:");
+  console.log("1. Añadir tarea");
+  console.log("2. Eliminar tarea");
+  console.log("3. Marcar tarea como completada");
+  console.log("4. Salir");
 
-rl.question("Seleccione una opción: ", (option) => {
-  switch (option) {
-    case "1":
-      rl.question("Indicador de la tarea: ", (indicador) => {
-        rl.question("Descripción de la tarea: ", (descripcion) => {
-          addTask(indicador, descripcion);
-          console.log("Tarea añadida exitosamente.");
-          rl.close();
+  rl.question("Seleccione una opción: ", (option) => {
+    switch (option) {
+      case "1":
+        rl.question("Indicador de la tarea: ", (indicador) => {
+          rl.question("Descripción de la tarea: ", (descripcion) => {
+            addTask(indicador, descripcion);
+            console.log("Tarea añadida exitosamente.");
+            showOptions(); // Mostrar opciones nuevamente
+          });
         });
-      });
-      break;
-    case "2":
-      rl.question("Índice de la tarea a eliminar: ", (index) => {
-        deleteTask(index);
-        console.log("Tarea eliminada exitosamente.");
-        rl.close();
-      });
-      break;
-    case "3":
-      rl.question("Índice de la tarea a marcar como completada: ", (index) => {
-        toggleTask(index);
-        console.log("Tarea marcada como completada exitosamente.");
-        rl.close();
-      });
-      break;
-    default:
-      console.log("Opción inválida. Por favor, seleccione una opción válida.");
-      rl.close();
-      break;
-  }
-});
+        break;
+      case "2":
+        rl.question("Índice de la tarea a eliminar: ", (index) => {
+          deleteTask(index);
+          console.log("Tarea eliminada exitosamente.");
+          showOptions(); // Mostrar opciones nuevamente
+        });
+        break;
+      case "3":
+        rl.question(
+          "Índice de la tarea a marcar como completada: ",
+          (index) => {
+            toggleTask(index);
+            console.log("Tarea marcada como completada exitosamente.");
+            showOptions(); // Mostrar opciones nuevamente
+          }
+        );
+        break;
+      case "4":
+        rl.close(); // Salir del programa
+        break;
+      default:
+        console.log(
+          "Opción inválida. Por favor, seleccione una opción válida."
+        );
+        showOptions(); // Mostrar opciones nuevamente
+        break;
+    }
+  });
+}
+
+console.log("Bienvenido a la gestión de tareas.");
+showOptions(); // Mostrar opciones disponibles inicialmente
