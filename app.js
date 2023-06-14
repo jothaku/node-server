89915e4ba300ba786e8737a859458ebe5fd58900
Jothaku <jothagf@gmail.com>
2023-06-14 10:55:04 -0500

    asynec y promises

--- app.js
+++ app.js
@@ -8,25 +8,38 @@ const rl = readline.createInterface({
 const tasks = [];
 
 function addTask(indicador, descripcion, completado = false) {
-  const task = {
-    indicador,
-    descripcion,
-    completado,
-  };
+  return new Promise((resolve, reject) => {
+    const task = {
+      indicador,
+      descripcion,
+      completado,
+    };
 
-  tasks.push(task);
+    tasks.push(task);
+    resolve();
+  });
 }
 
 function deleteTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-  }
+  return new Promise((resolve, reject) => {
+    if (index >= 0 && index < tasks.length) {
+      tasks.splice(index, 1);
+      resolve();
+    } else {
+      reject("Índice de tarea inválido.");
+    }
+  });
 }
 
 function toggleTask(index) {
-  if (index >= 0 && index < tasks.length) {
-    tasks[index].completado = !tasks[index].completado;
-  }
+  return new Promise((resolve, reject) => {
+    if (index >= 0 && index < tasks.length) {
+      tasks[index].completado = !tasks[index].completado;
+      resolve();
+    } else {
+      reject("Índice de tarea inválido.");
+    }
+  });
 }
 
 console.log("Bienvenido a la gestión de tareas.");
@@ -35,34 +48,62 @@ console.log("1. Añadir tarea");
 console.log("2. Eliminar tarea");
 console.log("3. Marcar tarea como completada");
 
-rl.question("Seleccione una opción: ", (option) => {
+async function askQuestion(question) {
+  return new Promise((resolve, reject) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+async function main() {
+  const option = await askQuestion("Seleccione una opción: ");
+
   switch (option) {
     case "1":
-      rl.question("Indicador de la tarea: ", (indicador) => {
-        rl.question("Descripción de la tarea: ", (descripcion) => {
-          addTask(indicador, descripcion);
-          console.log("Tarea añadida exitosamente.");
-          rl.close();
-        });
-      });
+      const indicador = await askQuestion("Indicador de la tarea: ");
+      const descripcion = await askQuestion("Descripción de la tarea: ");
+      try {
+        await addTask(indicador, descripcion);
+        console.log("Tarea añadida exitosamente.");
+      } catch (error) {
+        console.log("Error al añadir la tarea:", error);
+      }
+      rl.close();
       break;
     case "2":
-      rl.question("Índice de la tarea a eliminar: ", (index) => {
-        deleteTask(index);
+      const deleteIndex = await askQuestion("Índice de la tarea a eliminar: ");
+      try {
+        await deleteTask(deleteIndex);
         console.log("Tarea eliminada exitosamente.");
-        rl.close();
-      });
+      } catch (error) {
+        console.log("Error al eliminar la tarea:", error);
+      }
+      rl.close();
       break;
     case "3":
-      rl.question("Índice de la tarea a marcar como completada: ", (index) => {
-        toggleTask(index);
+      const toggleIndex = await askQuestion(
+        "Índice de la tarea a marcar como completada: "
+      );
+      try {
+        await toggleTask(toggleIndex);
         console.log("Tarea marcada como completada exitosamente.");
-        rl.close();
-      });
+      } catch (error) {
+        console.log("Error al marcar como completada la tarea:", error);
+      }
+      rl.close();
       break;
     default:
       console.log("Opción inválida. Por favor, seleccione una opción válida.");
       rl.close();
       break;
   }
-});
+}
+
+main()
+  .then(() => {
+    console.log("Programa finalizado.");
+  })
+  .catch((error) => {
+    console.log("Error en el programa:", error);
+  });
